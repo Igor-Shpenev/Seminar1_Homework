5ab636ec4c34afe4310254465e5454c19fb89d8a
Igor Shpenev <vdvshneek@yandex.ru>
2022-10-15 13:15:07 +0500

    Completed task number for

---
+++ Task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Input int number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while (count <= num)
+{
+    Console.WriteLine(count);
+    count = count + 2;
+}
+
+
